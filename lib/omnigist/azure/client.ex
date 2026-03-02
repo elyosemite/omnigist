b51ea6b9992fe60297b5b5ebeee21070bd8309f9
Yuri Melo <yurifullstack@gmail.com>
2026-03-01 23:04:58 -0300

    feat(azure): add Azure client module for API requests

---
+++ lib/omnigist/azure/client.ex
@@ -0,0 +1,30 @@
+defmodule Omnigist.Azure.Client do
+  @base_url "https://dev.azure.com"
+
+  def get(path, token) do
+    url = String.to_charlist("#{@base_url}#{path}")
+
+    credentials = Base.encode64(":" <> token)
+
+    headers = [
+      { ~c"Authorization", ~c"Basic #{credentials}" },
+      { ~c"Accept", ~c"application/json" },
+      { ~c"User-Agent", ~c"omnigist" }
+    ]
+
+    :httpc.request(:get, {url, headers}, [], [])
+    |> handle_response()
+  end
+
+  defp handle_response({:ok, {{_, 200, _}, _headers, body}}) do
+    {:ok, Jason.decode!(body)}
+  end
+
+  defp handle_response({:ok, {{_, status, _}, _headers, body}}) do
+    {:error, "Azure DevOps returned #{status}: #{body}"}
+  end
+
+  defp handle_response({:error, reason}) do
+    {:error, reason}
+  end
+end
