a3036fb337118055dfe0d0d14ab5ced2fd29c260
Yuri Melo <yurifullstack@gmail.com>
2026-03-02 21:31:52 -0300

    feat(azure): update list_projects and list_repositories functions for API integration

--- lib/omnigist/azure.ex
+++ lib/omnigist/azure.ex
@@ -2,8 +2,16 @@ defmodule Omnigist.Azure do
   alias Omnigist.Azure.{Client, Project, Repository}
 
   def list_projects(org, token) do
-    case Client.get("", token) do
+    case Client.get("/#{org}/_apis/projects?api-version=7.1", token) do
       {:ok, %{"value" => items}} -> {:ok, Enum.map(items, &parse_project/1)}
+      {:error, reason} -> {:error, reason }
+    end
+  end
+
+  def list_repositories(org, project, token) do
+    case Client.get("/#{org}/#{project}/_apis/git/repositories?api-version=7.1", token) do
+      {:ok, %{"value" => items}} -> {:ok, Enum.map(items, &parse_repository/1)}
+      {:error, reason} -> {:error, reason }
     end
   end
 
@@ -15,6 +23,18 @@ defmodule Omnigist.Azure do
       url:         data["url"],
       state:       data["state"],
       visibility:  data["visibility"]
+    }
+  end
+
+  defp parse_repository(data) do
+    %Repository{
+      id: data["id"],
+      name: data["name"],
+      url: data["url"],
+      ssh_url: data["sshUrl"],
+      default_branch: data["defaultBranch"],
+      size: data["size"],
+      project_name: get_in(data, ["project", "name"])
     }
   end
 end
