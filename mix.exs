dcb9764aaeb893a5ac4c962e5aadda921b1c4b7b
Yuri Melo <yurifullstack@gmail.com>
2026-03-12 22:22:17 -0300

    fix(mix): add :inets to extra applications in the application configuration

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Omnigist.MixProject do
   def application do
     [
       mod: {Omnigist.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
